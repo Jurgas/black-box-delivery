92f248dbefff465f88679d05f3f1b17d3e725924
jurgas <Sebastianjurga7@gmail.com>
2020-10-27 16:56:15 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bbd-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'black-box-delivery';
+}
