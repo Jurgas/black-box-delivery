e4f4f44872594d026be5daef23d9d3213c2bcff1
jurgas <Sebastianjurga7@gmail.com>
2020-11-24 03:16:09 +0100

    add auth-guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,14 @@ import {RegisterComponent} from './components/register/register.component';
 import {LoginComponent} from './components/login/login.component';
 import {LogoutComponent} from './components/logout/logout.component';
 import {DashboardComponent} from './components/dashboard/dashboard.component';
+import {AuthGuard} from './services/auth-guard.service';
 
 const routes: Routes = [
   {path: '', redirectTo: '/sender/dashboard', pathMatch: 'full'},
   {path: 'sender/register', component: RegisterComponent},
   {path: 'sender/login', component: LoginComponent},
   {path: 'sender/logout', component: LogoutComponent},
-  {path: 'sender/dashboard', component: DashboardComponent}
+  {path: 'sender/dashboard', component: DashboardComponent, canActivate: [AuthGuard]}
 ];
 
 @NgModule({
---
+++ src/app/services/auth-guard.service.ts
@@ -0,0 +1,25 @@
+import {Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
+import {Observable} from 'rxjs/observable';
+import {AuthService} from './auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private router: Router,
+              private authService: AuthService) {
+  }
+
+  canActivate(route: ActivatedRouteSnapshot,
+              state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return new Observable<boolean | UrlTree>(obs => {
+      this.authService.loggedIn().subscribe(() => {
+        obs.next(true);
+      }, () => {
+        obs.next(this.router.createUrlTree(['/sender/login']));
+      });
+    });
+  }
+}
